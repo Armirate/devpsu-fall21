1b81cb5c0840f1d0cce265793f88be3c3ba796af
Armirate <sorawit.mattra@gmail.com>
2021-10-12 19:33:47 -0400

    Adding some feeling to files

--- testing.c
+++ testing.c
@@ -2,5 +2,9 @@
 
 int main(){
   printf("Hello world");
+  int a = 5;
+  int b = 2;
+  int c = 10;
+  printf("%d",a+b+c);
   return 0;
 }
--- testing.py
+++ testing.py
@@ -1,1 +1,9 @@
+def feeling(txt):
+    if txt == 'love':
+        return 'I feel the same way.'
+    elif txt == 'happy':
+        return 'I am glad.'
+    elif txt == 'sad':
+        return 'I am here if you need me.'
+
 print(f"Hello world")
